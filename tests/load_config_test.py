68fb618b8142e1b45d7a1e19ea9a5a9bbb206144
okstyr <amuhrer@gmail.com>
2019-07-14 18:11:48 +1000

    Add the ability to configure modifier keys
    You can configure one (or more) buttons such that when they are
    held down, all the other buttons are remapped to different keys.
    
    For example when you hold down the space key on a nostromo game pad,
    instead of sending a space or some other key, instead you can remap
    the rest of the keys to numeric keys instead of WASD.

--- tests/load_config_test.py
+++ tests/load_config_test.py
@@ -47,11 +47,34 @@ class TestLoadConfig(unittest.TestCase):
     def test_accepts_other_parameters(self):
         mapping = remapping('config.yaml', ecodes.KEY_E)
         self.assertEqual("[{code: 30,param1: p1,param2: p2}]", pretty(mapping))
+    def test_accepts_modifier(self):
+        mapping = remapping('config.yaml', ecodes.KEY_Z)
+        self.assertEqual("[{modifier_group: mod1}]", pretty(mapping))
+    def test_mod_group_supports_simple_notation(self):
+        mapping = modified_remapping('config.yaml', ecodes.KEY_A)
+        self.assertEqual("[{code: 33}]", pretty(mapping))
+    def test_mod_group_supports_advanced_notation(self):
+        mapping = modified_remapping('config.yaml', ecodes.KEY_B)
+        self.assertEqual("[{'code': 33}]", str(mapping))
+    def test_mod_group_resolves_single_value(self):
+        mapping = modified_remapping('config.yaml', ecodes.KEY_C)
+        self.assertEqual("[{code: 33,value: [2]}]", pretty(mapping))
+    def test_mod_group_accepts_multiple_values(self):
+        mapping = modified_remapping('config.yaml', ecodes.KEY_D)
+        self.assertEqual("[{code: 33,value: [1, 3]}]", pretty(mapping))
+    def test_mod_group_accepts_other_parameters(self):
+        mapping = modified_remapping('config.yaml', ecodes.KEY_E)
+        self.assertEqual("[{code: 33,param3: p1,param4: p2}]", pretty(mapping))
 
 def remapping(config_name, code):
     config_path = '{}/{}'.format(spec_dir, config_name)
     config = load_config(config_path)
     return config['devices'][0]['remappings'].get(code)
+
+def modified_remapping(config_name, code):
+    config_path = '{}/{}'.format(spec_dir, config_name)
+    config = load_config(config_path)
+    return config['devices'][0]['modifier_groups']['mod1'].get(code)
 
 def pretty(mappings):
     def prettymapping(mapping):
