5243b4447d05ccfc2742c7961c43be0b2ac4d13c
Daniel Joyce <daniel.a.joyce@gmail.com>
2020-08-14 16:03:12 -0700

    Rename file

--- build-binary.sh
+++ build-binary.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Builds a single binary executable version of evdevremapkeys
+
+rm -rf ./build ./dist ./bundle
 
 if [[ -z "${VIRTUAL_ENV}" ]]; then
     virtualenv -p python3 venv
@@ -6,12 +9,11 @@ fi
 
 source ./venv/bin/activate
 
-pip install -r pip-requirements.txt
+pip install .
+pip install -e '.[binary]'
 
 # Clean build
 # pip install --force-reinstall --no-cache-dir --no-binary :all: -r requirements.txt
-
-rm -rf ./build ./dist ./bundle
 
 pyinstaller --onefile ./evdevremapkeys/evdevremapkeys.py
 
