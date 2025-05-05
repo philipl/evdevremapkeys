5feee2cc5e0a3711252155c01d9bdeb56bf23a2e
Philip Langdale <philipl@overt.org>
2025-05-05 12:41:25 -0700

    build: switch to uv to manage our build
    This has been a long time coming, but I have to sort out the build and
    packaging story, and now that `uv` is a thing, it provides an easy way
    to do this.
    
    This change heavily streamlines how building and packaging happens.
    
    * Switch to pyproject.toml and retire setup.cfg
    * Use uv to manage dependencies and to run tools
    * Switch to hatch for building the sdist, and retire setuptools
    * Switch to ruff for linting and formatting
    
    The minimum python version is also updated to 3.8 as the more modern
    versions of pyinstaller require it. It seems like a reasonable thing to do
    given how much time has passed, in any case.

--- build-binary.sh
+++ build-binary.sh
@@ -3,20 +3,8 @@
 
 rm -rf ./build ./dist ./bundle
 
-if [[ -z "${VIRTUAL_ENV}" ]]; then
-    virtualenv -p python3 venv
-fi
-
-source ./venv/bin/activate
-
-pip install .
-pip install -e '.[binary]'
-
-# Clean build
-# pip install --force-reinstall --no-cache-dir --no-binary :all: -r requirements.txt
-
-pyinstaller --onefile ./evdevremapkeys/evdevremapkeys.py
+uv run pyinstaller --onefile ./evdevremapkeys/evdevremapkeys.py
 
 mkdir bundle
 
-staticx ./dist/evdevremapkeys ./bundle/evdevremapkeys
+uv run staticx ./dist/evdevremapkeys ./bundle/evdevremapkeys
